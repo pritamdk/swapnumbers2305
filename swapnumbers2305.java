cc16ee72aee603b8d9311a2f2fe584cee0ad7d91
Pritamdk <pritamdk1994@gmail.com>
2022-05-23 17:47:30 +0530

    added : swapprogram

---
+++ swapnumbers2305.java
@@ -0,0 +1,26 @@
+package swapnumbers;
+
+public class swapnumbers2305 {
+
+	public static void main(String[] args) {
+		int num1 = 3; 
+		int num2 = 8;
+			
+			System.out.println("Before Swapping");
+			System.out.println("Value of num1 is:" + num1);
+			System.out.println("Value of num2 is:" + num2);
+			
+		swap(num1 ,num2);
+				
+	}
+	private static void swap(int num1, int num2){
+		int temp = num1;
+		num1 = num2;
+		num2 = temp;
+		
+		System.out.println("After Swapping");
+		System.out.println("Value of num1 is:" + num1);
+		System.out.println("Value of num2 is:" + num2);
+	}
+
+}
